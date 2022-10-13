5c562f6432efbd3102635c148e7f6e11017d2dc7
fan9704 <cxz123499@gmail.com>
2022-10-13 23:10:18 +0800

    Update MQTT Config

---
+++ src/utils/mqtt.js
@@ -0,0 +1,5 @@
+export const MQTT_SERVICE = 'ws://127.0.0.1:15674/ws' // mqtt服務地址
+export const MQTT_USERNAME = 'guest' // mqtt連接用戶名
+export const MQTT_PASSWORD = 'guest' // mqtt連接密碼
+export const MQTT_host = 'tx' // mqtt交換機
+export const MQTT_topic = 'push_message_topic' // 訂閱頻道
