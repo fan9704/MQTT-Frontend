f4b33971b64476b2f72f9d7949a5b3ffb01f9bc8
fan9704 <cxz123499@gmail.com>
2023-03-20 11:11:42 +0800

    Complete PWA Configuration

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,23 @@ import App from './App.vue'
 import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import { loadFonts } from './plugins/webfontloader'
-import '@mdi/font/css/materialdesignicons.css'
+import { loadFonts } from './plugins/webfontloader';
+import '@mdi/font/css/materialdesignicons.css';
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register';
+registerSW({
+    onNeedRefresh () {},
+    onOfflineReady () {}
+})()
 
 const vuetify = createVuetify({
     components,
@@ -21,4 +31,6 @@ app.use(vuetify);
 app.use(router)
 app.use(VueAxios, axios);
 app.use(loadFonts);
+app.use(AOS.init());
+app.use(VueSweetalert2);
 app.mount('#app');
