563cbd9baf96a39d14e9fd6153d4b9e2ed12127d
fan9704 <fan9704@users.noreply.github.com>
2023-07-20 03:14:10 +0800

    deploy: a58ec4388b17288fc1b07c5c66126f3b49d47ff2

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA1FevAEUfg7u_tmhjPKGLJfVFBiQaghCY",
+    authDomain: "petmonitoringsystem-729da.firebaseapp.com",
+    projectId: "petmonitoringsystem-729da",
+    storageBucket: "petmonitoringsystem-729da.appspot.com",
+    messagingSenderId: "218937674834",
+    appId: "1:218937674834:web:2961695e7663494adb638b",
+    measurementId: "G-D4TRM99C0G"
+}
+const app = firebase.initializeApp(firebaseConfig);
